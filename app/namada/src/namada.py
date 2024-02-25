5f3cfb8873ea4a27dcb4df62eb042bc17e2d37e5
sam <>
2024-02-25 16:28:02 +0200

    Update namada.py

--- app/namada/src/namada.py
+++ app/namada/src/namada.py
@@ -1,12 +1,14 @@
 import os
 
 class Namada(object):
+    status = "Comming soon..."
+    node = ''
+    alias = ''
+
     """docstring for Namada Python package"""
     def __init__(self, node, *args, **kwargs):
         super(Namada, self).__init__(*args, **kwargs)
         self.node = node
-        self.status = "Comming soon..."
-        print(self.status)
         
     def test(self):
         return os.system("namada wallet list")

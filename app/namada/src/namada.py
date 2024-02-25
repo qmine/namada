720e8fa414471074e79a3addcf67149c14811512
sam <>
2024-02-25 16:07:10 +0200

    Update namada.py

--- app/namada/src/namada.py
+++ app/namada/src/namada.py
@@ -1,7 +1,8 @@
 class Namada(object):
     """docstring for Namada Python package"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self, node, *args, **kwargs):
         super(Namada, self).__init__(*args, **kwargs)
+        self.node = node
         self.status = "Comming soon..."
         print(self.status)
         

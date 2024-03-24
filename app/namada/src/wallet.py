daf68157cd5bb3d5694960ecebb197657c14963f
sam <>
2024-03-24 14:41:07 +0200

    Update wallet.py

--- app/namada/src/wallet.py
+++ app/namada/src/wallet.py
@@ -10,10 +10,10 @@ class Wallet(object):
         """
         --chain-id <chain-id>  The chain ID.
         --transparent          List transparent keys / addresses only.
-        --base-dir <base-dir>  The base directory is where the nodes, client and wallet configuration and state is stored. This value can also be set via `NAMADA_BASE_DIR` environment variable, but the argument takes precedence, if specified. Defaults to `$XDG_DATA_HOME/namada` (`$HOME/.local/share/namada` where `XDG_DATA_HOME` is unset) on Unix,`$HOME/Library/Application Support/Namada` on Mac,and `%AppData%\Namada` on Windows.
+        --base-dir <base-dir>  The base directory is where the nodes, client and wallet configuration and state is stored.
         --shielded             List keys / addresses of the shielded pool only.
         --keys                 List keys only.
-        --wasm-dir <wasm-dir>  Directory with built WASM validity predicates, transactions. This value can also be set via `NAMADA_WASM_DIR` environment variable, but the argument takes precedence, if specified.
+        --wasm-dir <wasm-dir>  Directory with built WASM validity predicates, transactions.
         --addr                 List addresses only.
         --pre-genesis          Dispatch pre-genesis specific logic.
         --decrypt              Decrypt keys that are encrypted.
